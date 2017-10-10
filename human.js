e96fc0f7fe9851908733b175f5f051806224bd58
erdivartanovich <erdivartanovich@gmail.com>
2017-10-10 12:45:35 +0700

    reset class definition

--- human.js
+++ human.js
@@ -1,17 +1,4 @@
 const Human = function(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName; 
-}
-
-Human.prototype.completeName = function() {
-    var name
-    this.lastName = this.lastName ? this.lastName : this.firstName
-    this.firstName = this.firstName ? this.firstName : this.lastName
-    name = [this.firstName, this.lastName].join(" ")
-    if(!name.trim()) {
-        return 'unknown'
-    }
-    return name
 }
 
 module.exports = Human
